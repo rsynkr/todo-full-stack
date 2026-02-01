60c210ea0237b1df243f402816a83852b5620604
unknown <narkarrishi16@gmail.com>
2026-02-01 18:55:09 +0530

    changes

--- src/routes/authroutes.js
+++ src/routes/authroutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import db from '../db.js';
+
 import { hash } from 'crypto';
 import prisma from '../prismaclient.js';
 const router = express.Router();
--- src/routes/todoroutes.js
+++ src/routes/todoroutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import db from "../db.js"
+
 import prisma from '../prismaclient.js';
 const router = express.Router();
 
